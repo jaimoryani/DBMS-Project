64702687014d71506a4ca416b8027f36d06644a3
Kuber144 <kuberjain144@gmail.com>
2023-02-07 21:36:24 +0530

    php changes, js changes

--- php/connection.php
+++ php/connection.php
@@ -1,12 +1,11 @@
 <?php
 $host="localhost";
 $user="root";
-$password="jaimoryani";
-$db_name="dbms";
+$password="12345";
+$db_name="feedback_management";
 
 $con=mysqli_connect($host,$user,$password,$db_name);
 if(mysqli_connect_errno()){
     die("Failed to connect");
 }
-else echo("fegrwbfe");
 ?>
