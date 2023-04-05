83ea7be5b411c7a195ad313b9de15275b08ba690
Ishaan Oberoi <ishan23oberoi@gmail.com>
2023-04-06 02:31:21 +0530

    Working on admin

--- php/connection.php
+++ php/connection.php
@@ -2,7 +2,7 @@
 $host="localhost";
 $user="root";
 $password="ishaan930838";
-$db_name="feedback_management";
+$db_name="feedback_management_grp1";
 
 $con=mysqli_connect($host,$user,$password,$db_name);
 if(mysqli_connect_errno()){
