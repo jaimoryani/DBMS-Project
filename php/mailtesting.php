325b80ed7d23a7cc2027e58e4726bb595a4ba051
Ishaan23o <96721096+Ishaan23o@users.noreply.github.com>
2023-04-12 02:06:21 +0530

    small changes

--- php/mailtesting.php
+++ php/mailtesting.php
@@ -1,4 +1,5 @@
 <?php
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
@@ -7,7 +8,7 @@ use PHPMailer\PHPMailer\SMTP;
 // require 'PHPMailer/PHPMailer.php';
 //     require("PHPMailer/SMTP.php");
 // use PHPMailer\SMTP;
-function sendMail()
+function sendPassword($email, $password)
 {
     require("PHPMailer/PHPMailer.php");
     require("PHPMailer/SMTP.php");
@@ -15,43 +16,34 @@ function sendMail()
     $mail = new PHPMailer(true);
 
     try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'replytofeedbackmanagement@gmail.com';                     //SMTP username
-    $mail->Password   = 'fwbynkkrcnktnali';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'replytofeedbackmanagement@gmail.com';                     //SMTP username
+        $mail->Password   = 'fwbynkkrcnktnali';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-    //Recipients
-    $mail->setFrom('replytofeedbackmanagement@gmail.com', 'Mailer');
-    $mail->addAddress('iit2021184@iiita.ac.in');     //Add a recipient
-    $mail->addAddress('iit2021155@iiita.ac.in');               //Name is optional
-    $mail->addAddress('iit2021163@iiita.ac.in');               //Name is optional
-    $mail->addAddress('iit2021122@iiita.ac.in');  
-    $mail->addAddress('iit2021109@iiita.ac.in');               //Name is optional
-                 //Name is optional
+        //Recipients
+        $mail->setFrom('replytofeedbackmanagement@gmail.com', 'Mailer');
+        $mail->addAddress($email);     //Add a recipient
+        //Name is optional
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Testing';
-    $mail->Body    = 'Hellooooooooooooooooooooo hdwufbuebfeufeufu Testing \n wifwifbwifbiw';
-
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Regarding enrollment to the feedback Portal';
+        $mail->Body    = "Hey you have been added to the feedback management system, Your password is $password";
+        $mail->send();
+        echo 'Message has been sent';
+        return true;
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+        return false;
+    }
 }
-}
-
-sendMail()
-
-
-
-?>
