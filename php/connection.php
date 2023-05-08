30ac0a441e9d95b22b9dbacd013a8f1157f9e3ba
jaimoryani <jaimoryanir@gmail.com>
2023-05-09 05:26:38 +0530

    front

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
 $host="localhost";
 $user="root";
-$password="12345";
-$db_name="feedback_management_grp1";
+$password="jaimoryani";
+$db_name="feedback";
 
 $con=mysqli_connect($host,$user,$password,$db_name);
 if(mysqli_connect_errno()){
