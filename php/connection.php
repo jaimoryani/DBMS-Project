26f9e75a9c34f9ab6c84c718d178c47ad9b2ff41
Kuber144 <kuberjain144@gmail.com>
2023-05-12 01:40:33 +0530

    P1_ prefix added

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
 $host="localhost";
-$user="root";
-$password="jaimoryani";
-$db_name="feedback";
+$user="fcsldba";
+$password="Junaid_123";
+$db_name="fcsldb";
 
 $con=mysqli_connect($host,$user,$password,$db_name);
 if(mysqli_connect_errno()){
