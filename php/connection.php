ec20721ee96621f987f418f1835e47e85a6d3d4f
Ishaan23o <96721096+Ishaan23o@users.noreply.github.com>
2023-06-16 23:47:32 +0530

    Single Feedback per student implemented

--- php/connection.php
+++ php/connection.php
@@ -1,10 +1,10 @@
 <?php
-$host="localhost";
-$user="fcsldba";
-$password="Junaid_123";
-$db_name="fcsldb";
+$host = "localhost";
+$user = "root";
+$password = "12345";
+$db_name = "fcsldb";
 
-$con=mysqli_connect($host,$user,$password,$db_name);
-if(mysqli_connect_errno()){
+$con = mysqli_connect($host, $user, $password, $db_name);
+if (mysqli_connect_errno()) {
     die("Failed to connect");
 }
